04645038b8ca57d7ef703d4ba60ae117c0572461
liuzheng712 <liuzheng712@gmail.com>
2013-12-25 17:38:27 +0800

    task似乎还有点问题

--- mycode/tasks.py
+++ mycode/tasks.py
@@ -6,7 +6,15 @@
 
 
 from celery.task import task
+import loginweibo as Lwb
+import jiexi
+import file
 
 @task
-def add(x, y):
-    return x + y
+def write(session, who):
+    page, who = Lwb.getPage(session, 'http://weibo.com/' + who)
+    WB_text, WB_time, WB_comefrom, WB_like, WB_pinlun, WB_forward, WB_mid = jiexi.detail(page)
+    for i in range(0, len(WB_text)):
+        file.inputweibo(who, WB_text[i], WB_time[i], WB_mid[i],
+                WB_comefrom[i], WB_like[i], WB_forward[i], WB_pinlun[i])
+    print 'Success!'
