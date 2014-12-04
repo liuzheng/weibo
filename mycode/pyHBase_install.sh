b1c42d5ac7afc8cf425159b4573f13d46e688f50
liuzheng712 <liuzheng712@gmail.com>
2014-12-04 18:09:15 +0800

    dd

--- mycode/pyHBase_install.sh
+++ mycode/pyHBase_install.sh
@@ -2,4 +2,4 @@ sudo add-apt-repository "deb http://apt.pub.simplegeo.com/ubuntu foss-developmen
 sudo aptitude update
 sudo aptitude install simplegeo-keyring
 sudo aptitude update
-sudo aptiutde install python-hbase
+sudo aptitude install python-hbase
